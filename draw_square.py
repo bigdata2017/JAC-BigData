95afc5629410d4a51c63b037ac0c5761021a40eb
bigdata2017 <saifbigdata2017@gmail.com>
2017-06-03 16:01:58 -0400

    Add files via upload

--- draw_square.py
+++ draw_square.py
@@ -2,9 +2,13 @@ import turtle
 
 def draw_square():
     window = turtle.Screen()
-    window.bgcolor("red")
+    window.bgcolor("pink")
  
     sqr = turtle.Turtle()
+    sqr.shape("triangle")
+    sqr.color("yellow")
+    sqr.speed (2)
+              
     sqr.forward(100)
     sqr.right(90)
     sqr.forward(100)
