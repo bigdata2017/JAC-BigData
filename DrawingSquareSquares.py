9c5274be52d23910c402ce319f1ca1605344f26c
bigdata2017 <saifbigdata2017@gmail.com>
2017-06-10 14:22:47 -0400

    Add files via upload

---
+++ DrawingSquareSquares.py
@@ -0,0 +1,28 @@
+import turtle
+
+
+def draw_square(some_turtle):
+    for i in range(0,4):
+        some_turtle.forward(20)
+        some_turtle.right(90)           
+
+def draw_art():        
+    # Create window screen
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    # Create turtle "brad"
+    brad = turtle.Turtle()
+    brad.shape("arrow")
+    brad.color("yellow")
+    brad.speed(10)
+
+    # Draw 50 squares
+    for i in range (0, 4):
+        draw_square(brad)
+        brad.forward(50)
+        brad.left(90)
+
+    window.exitonclick()
+
+draw_art()
